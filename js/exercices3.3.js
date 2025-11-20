6bf08060317cc5a976e5df27b03c4b9f45cd0fd5
Kisame25021990 <bourlonj1990@gmail.com>
2025-11-20 13:33:28 +0100

    Add files via upload

---
+++ js/exercices3.3.js
@@ -0,0 +1,8 @@
+let original = [1, 2, 3, 4];
+let copie = [];
+
+for (let elem of original) {
+  copie.push(elem); // ajoute chaque élément dans le nouveau tableau
+}
+
+console.log(copie); // [1, 2, 3, 4]
